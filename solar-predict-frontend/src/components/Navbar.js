0e6ff7fe733bbf36a2745b70f7a3c898f7e9c8a7
nidhi <agrawalnidhi@1710.com>
2026-05-20 22:24:09 +0530

    Fix navbar unused import

--- solar-predict-frontend/src/components/Navbar.js
+++ solar-predict-frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 import "./Navbar.css";
-import { FiSun, FiSettings, FiChevronDown, FiMenu, FiX } from "react-icons/fi";
+import { FiSun, FiSettings, FiMenu, FiX } from "react-icons/fi";
 
 function Navbar() {
   const location = useLocation();
