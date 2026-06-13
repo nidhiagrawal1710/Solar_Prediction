42fd8329ed92ece577e2cb46437322ea73c75b66
nidhi <agrawalnidhi1710@gmail.com>
2026-06-14 00:33:57 +0530

    Updated RestCountries API integration 1

--- solar-predict-frontend/src/components/Calculator.js
+++ solar-predict-frontend/src/components/Calculator.js
@@ -20,7 +20,7 @@ export default function SolarCalculatorMain({ setSolarData, setLastUpdated ,setF
   const [locationLabel, setLocationLabel] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const API_URL = process.env.REACT_APP_API_URL;
-  const RESTCOUNTRIES_KEY = process.env.REACT_APP_RESTCOUNTRIES_KEY;
+  // const RESTCOUNTRIES_KEY = process.env.REACT_APP_RESTCOUNTRIES_KEY;
 
 
   const calculatePanelArea = (numPanels, panelRating, efficiency = 0.18) => {
@@ -57,27 +57,19 @@ useEffect(() => {
   const loadCountries = async () => {
     try {
       const response = await fetch(
-        "https://api.restcountries.com/countries/v5?response_fields=names.common",
-        {
-          headers: {
-            'Authorization': `Bearer ${RESTCOUNTRIES_KEY}`
-          },
-        }
+        "https://countriesnow.space/api/v0.1/countries"
       );
 
       if (!response.ok) {
-        const error = await response.text();
-        throw new Error(`HTTP ${response.status}: ${error}`);
+        throw new Error(`HTTP ${response.status}`);
       }
 
       const result = await response.json();
 
-      const countries = result?.data?.objects || [];
-
-      const formatted = countries
+      const formatted = (result?.data || [])
         .map((country) => ({
-          label: country?.names?.common,
-          value: country?.names?.common,
+          label: country.country,
+          value: country.country,
         }))
         .filter((country) => country.label)
         .sort((a, b) => a.label.localeCompare(b.label));
@@ -90,8 +82,7 @@ useEffect(() => {
   };
 
   loadCountries();
-}, [RESTCOUNTRIES_KEY]);
-
+}, []);
 
 
   const fetchCities = async (countryName) => {
